0475e4a2e61aa20bc632962893e4cde7a000b03a
hylansilvadev <hylansilva@outlook.com>
2024-06-26 16:16:22 -0300

    start project

---
+++ src/main/java/com/hylansilvadev/api/hylansilvadevapi/HylansilvadevapiApplication.java
@@ -0,0 +1,13 @@
+package com.hylansilvadev.api.hylansilvadevapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HylansilvadevapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HylansilvadevapiApplication.class, args);
+	}
+
+}
